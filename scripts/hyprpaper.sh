c63bd3b4ae111dd7823ea20926fd55e7ede95342
Alexandre Doucet <alexandre.doucet6@gmail.com>
2025-05-10 07:46:35 -0300

    Updated hyprlock and hyprpaper to ensure more consistent function

--- scripts/hyprpaper.sh
+++ scripts/hyprpaper.sh
@@ -1,1 +1,21 @@
-/home/adoucet/.config/hypr/hyprpaper.sh
+#!/usr/bin/env bash
+
+sleep 2
+
+WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+# Get a list of wallpapers excluding the current one
+#WALLPAPERS=($(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")")) # for recursive search
+WALLPAPERS=($(find "$WALLPAPER_DIR" -maxdepth 1 -type f ! -name "$(basename "$CURRENT_WALL")"))
+
+if [[ ${#WALLPAPERS[@]} -eq 0 ]]; then
+  # If no other wallpapers exist, fallback to the current one
+  WALLPAPER="$CURRENT_WALL"
+else
+  # Otherwise, select a random wallpaper
+  WALLPAPER=$(printf "%s\n" "${WALLPAPERS[@]}" | shuf -n 1)
+fi
+
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
